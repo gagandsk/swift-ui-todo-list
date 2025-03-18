a66219b6189ddb6f7da8439be8346f16ad76727e
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-18 15:34:53 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Gagandeep Dass Kaur on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
